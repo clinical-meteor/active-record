bed2b151053c01ddc37c07157b235b80fb586b6a
Abigail Watson <awatson1978@gmail.com>
2015-10-18 21:00:04 -0700

    updated git repo

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:active-record',
-  version: '1.0.7',
+  version: '1.0.8',
   summary: 'ActiveRecord component for the ClinicalFramework; provides CRUD pattern using Autoform.',
   git: 'http://github.com/clinical-meteor/clinical-active-record/',
   documentation: 'README.md'
